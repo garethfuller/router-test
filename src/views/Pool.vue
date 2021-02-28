5b376d405fc7a44978fa9f4cb9d593fce36fb364
Gareth Fuller <gareth.a.f@gmail.com>
2021-02-28 15:38:03 +0000

    Init

---
+++ src/views/Pool.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Pool id: {{ $route.params.id }}
+  </div>
+</template>
+
+<script>
+export default {
+  beforeMount () {
+    console.log(this.$route.params.id)
+  }
+}
+</script>
